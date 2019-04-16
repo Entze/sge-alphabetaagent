ed7e34142bf7b4c545969688fe3bdc065f8f1fce
Lukas Grassauer <lukas@grassauer.eu>
2019-04-16 22:38:59 +0200

    remove gradle stuff and implement AlphaBetaSearch

--- src/main/java/dev/entze/sge/agent/alphabetaagent/AlphaBetaAgent.java
+++ src/main/java/dev/entze/sge/agent/alphabetaagent/AlphaBetaAgent.java
@@ -2,33 +2,48 @@ package dev.entze.sge.agent.alphabetaagent;
 
 import dev.entze.sge.agent.GameAgent;
 import dev.entze.sge.game.Game;
+import dev.entze.sge.util.pair.MutablePair;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.Random;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
-public class AlphaBetaAgent<G extends Game<? extends A, ?>, A> implements GameAgent<G, A> {
+public class AlphaBetaAgent<G extends Game<A, ?>, A> implements GameAgent<G, A> {
 
-  private long stopSearchTimeInNano;
+  public final long STOP_SEARCH_TIME_MULTIPLIER = 50L;
+  public final long STOP_SEARCH_TIME_DIVISOR = 100L;
 
+  public final long RESTART_SEARCH_MULTIPLIER = STOP_SEARCH_TIME_MULTIPLIER / 2;
+  public final long RESTART_SEARCH_DIVISOR = 100L;
+  private final Random random;
   private final Comparator<Game<A, ?>> gameUtilityComparator = Comparator
       .comparingDouble(o -> o.getUtilityValue());
-
   private final Comparator<Game<A, ?>> gameHeuristicComparator = Comparator
       .comparingDouble(o -> o.getHeuristicValue());
-
   private final Comparator<Game<A, ?>> gameComparator = gameUtilityComparator.reversed()
       .thenComparing(gameHeuristicComparator.reversed());
+  int[] stats;
+  private long startingTimeInNano;
+  private long stopSearchTimeInNano;
+  private double[] evaluationWeights;
+  private int playerNumber;
+  private int depth;
 
   public AlphaBetaAgent() {
-
+    stats = new int[64];
+    random = new Random();
   }
 
   @Override
-  public void setUp(int numberOfPlayers) {
+  public void setUp(int numberOfPlayers, int playerNumber) {
+    evaluationWeights = new double[numberOfPlayers];
+    Arrays.fill(evaluationWeights, (-1D));
+    evaluationWeights[playerNumber] = 1;
+    this.playerNumber = playerNumber;
+    depth = 10;
   }
 
   @Override
@@ -37,38 +52,83 @@ public class AlphaBetaAgent<G extends Game<? extends A, ?>, A> implements GameAg
 
   @Override
   public A computeNextAction(G game, long computationTime, TimeUnit timeUnit) {
-    long startingTimeInNano = System.nanoTime();
+    startingTimeInNano = System.nanoTime();
+    long computationTimeInNano = timeUnit.toNanos(computationTime);
     stopSearchTimeInNano = startingTimeInNano + Math
-        .max(timeUnit.toNanos((computationTime * 50L) / 100L) - 1000L, 0L);
+        .max(computationTimeInNano * STOP_SEARCH_TIME_DIVISOR / STOP_SEARCH_TIME_DIVISOR
+            - 1000L, 0L);
 
+    Set<A> possibleActions = game.getPossibleActions();
 
-    return null;
+    List<MutablePair<Game<A, ?>, NodeEvaluation>> childMap = new ArrayList<>(
+        possibleActions.size());
+
+    NodeEvaluation alpha = NodeEvaluation.NEGATIVE_INFINITY;
+    NodeEvaluation beta = NodeEvaluation.POSITIVE_INFINITY;
+
+    for (A possibleAction : possibleActions) {
+      Game<A, ?> child = game.doAction(possibleAction);
+      childMap.add(new MutablePair<>(child, new NodeEvaluation(child, evaluationWeights)));
+    }
+
+    childMap.sort((o1, o2) -> o2.getB().compareTo(o1.getB()));
+
+    int round = 0;
+    int lastDepth = 1;
+    while ((computationTimeInNano * RESTART_SEARCH_DIVISOR) / RESTART_SEARCH_MULTIPLIER
+        > currentComputationTime() && round < lastDepth && !isStopSearchTime()) {
+      depth = determineDepth(stats);
+      if (depth < 2) {
+        depth = (int) timeUnit.toSeconds(computationTime) / childMap.size();
+      }
+      depth = Math.min(Math.max(depth, lastDepth + 1), stats.length - 1);
+      for (MutablePair<Game<A, ?>, NodeEvaluation> child : childMap) {
+        NodeEvaluation evaluation = alphaBeta(child.getA(), depth - 1, alpha, beta);
+        if (!isStopSearchTime()) {
+          child.setB(evaluation);
+          if (NodeEvaluation.moreThanOrEqual(evaluation, alpha)) {
+            alpha = evaluation;
+          }
+        }
+      }
+
+      if (isStopSearchTime()) {
+        stats[depth - 1]--;
+      } else {
+        stats[depth]++;
+      }
+      lastDepth = depth;
+      round++;
+      childMap.sort((o1, o2) -> o2.getB().compareTo(o1.getB()));
+      alpha = childMap.get(0).getB();
+    }
+
+    return childMap.get(0).getA().getPreviousAction();
   }
 
 
-  //TODO: Recursive implementation -> don't care how many enemies
-  public NodeEvaluation alphaBeta(Game<A, ?> game, int depth,
-      Map<Integer, NodeEvaluation> alphaBetaMap) {
+  public NodeEvaluation alphaBeta(Game<A, ?> game, int depth, NodeEvaluation alpha,
+      NodeEvaluation beta) {
     if (depth <= 0 || game.isGameOver() || isStopSearchTime()) {
-      return new NodeEvaluation(game);
+      return new NodeEvaluation(game, evaluationWeights);
     }
 
     int currentPlayer = game.getCurrentPlayer();
-    NodeEvaluation value = new NodeEvaluation();
+    NodeEvaluation value = (currentPlayer == playerNumber ? NodeEvaluation.NEGATIVE_INFINITY
+        : NodeEvaluation.POSITIVE_INFINITY);
     List<Game<A, ?>> children = getChildren(game);
-    Set<Integer> nextPlayerCandidates = new TreeSet<>();
 
     for (Game<A, ?> child : children) {
-      nextPlayerCandidates.add(child.getCurrentPlayer());
-    }
-
-    nextPlayerCandidates.remove(currentPlayer);
-
-    for (Game<A, ?> child : children) {
-      value = NodeEvaluation.max(value, alphaBeta(child, depth - 1, alphaBetaMap));
-
-      //alphaBetaMap.put(currentPlayer, )
-
+      if (currentPlayer == playerNumber) { //check if is maximizing
+        value = NodeEvaluation.max(value, alphaBeta(child, depth - 1, alpha, beta));
+        alpha = NodeEvaluation.max(alpha, value);
+      } else {
+        value = NodeEvaluation.min(value, alphaBeta(child, depth - 1, alpha, beta));
+        beta = NodeEvaluation.min(beta, value);
+      }
+      if (NodeEvaluation.moreThanOrEqual(alpha, beta)) {
+        break;
+      }
     }
 
     return value;
@@ -87,9 +147,25 @@ public class AlphaBetaAgent<G extends Game<? extends A, ?>, A> implements GameAg
     return children;
   }
 
+  private int determineDepth(int[] stats) {
+    int max = Integer.MIN_VALUE;
+    int maxIndex = (-1);
+    for (int i = 0; i < stats.length; i++) {
+      if (stats[i] > max) {
+        maxIndex = i;
+        max = stats[i];
+      }
+    }
+    return maxIndex + random.nextInt(2);
+  }
 
   private boolean isStopSearchTime() {
-    return System.nanoTime() >= stopSearchTimeInNano;
+    return System.nanoTime() >= stopSearchTimeInNano || !Thread.currentThread().isAlive() || Thread
+        .currentThread().isInterrupted();
+  }
+
+  private long currentComputationTime() {
+    return System.nanoTime() - startingTimeInNano;
   }
 
 }
--- src/main/java/dev/entze/sge/agent/alphabetaagent/NodeEvaluation.java
+++ src/main/java/dev/entze/sge/agent/alphabetaagent/NodeEvaluation.java
@@ -5,15 +5,20 @@ import java.util.Objects;
 
 public class NodeEvaluation implements Comparable<NodeEvaluation> {
 
+  public static final NodeEvaluation NEGATIVE_INFINITY = new NodeEvaluation(
+      Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
+  public static final NodeEvaluation POSITIVE_INFINITY = new NodeEvaluation(
+      Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+
   private final double utility;
   private final double heuristic;
 
-  public NodeEvaluation(){
-    this(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
-  }
-
   public NodeEvaluation(Game<?, ?> game) {
     this(game.getUtilityValue(), game.getHeuristicValue());
+  }
+
+  public NodeEvaluation(Game<?, ?> game, double[] weights) {
+    this(game.getUtilityValue(weights), game.getHeuristicValue(weights));
   }
 
   public NodeEvaluation(double utility, double heuristic) {
@@ -21,39 +26,8 @@ public class NodeEvaluation implements Comparable<NodeEvaluation> {
     this.heuristic = heuristic;
   }
 
-  public double getUtility() {
-    return utility;
-  }
-
-  public double getHeuristic() {
-    return heuristic;
-  }
-
-  @Override
-  public int compareTo(NodeEvaluation o) {
-    int compare = Double.compare(this.utility, o.utility);
-    if (compare == 0) {
-      return Double.compare(this.heuristic, o.heuristic);
-    }
-    return compare;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    NodeEvaluation that = (NodeEvaluation) o;
-    return Double.compare(that.utility, utility) == 0 &&
-        Double.compare(that.heuristic, heuristic) == 0;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(utility, heuristic);
+  public NodeEvaluation(NodeEvaluation nodeEvaluation) {
+    this(nodeEvaluation.utility, nodeEvaluation.heuristic);
   }
 
   public static NodeEvaluation min(NodeEvaluation a, NodeEvaluation b) {
@@ -96,6 +70,41 @@ public class NodeEvaluation implements Comparable<NodeEvaluation> {
 
   public static boolean moreThan(NodeEvaluation a, NodeEvaluation b) {
     return a.compareTo(b) > 0;
+  }
+
+  public double getUtility() {
+    return utility;
+  }
+
+  public double getHeuristic() {
+    return heuristic;
+  }
+
+  @Override
+  public int compareTo(NodeEvaluation o) {
+    int compare = Double.compare(this.utility, o.utility);
+    if (compare == 0) {
+      return Double.compare(this.heuristic, o.heuristic);
+    }
+    return compare;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    NodeEvaluation that = (NodeEvaluation) o;
+    return Double.compare(that.utility, utility) == 0 &&
+        Double.compare(that.heuristic, heuristic) == 0;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(utility, heuristic);
   }
 
 }
